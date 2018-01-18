3a7cec38cf83cd56f956053a4a3e5605f8267657
James Chen <jianhua.chen@cocos2d-x.org>
2018-01-18 11:58:00 +0800

    Copy dragonbones vector before enumating the vector.

--- cocos/scripting/js-bindings/manual/jsb_dragonbones_manual.cpp
+++ cocos/scripting/js-bindings/manual/jsb_dragonbones_manual.cpp
@@ -461,12 +461,12 @@ bool register_all_dragonbones_manual(se::Object* obj)
         dragonBones::WorldClock::clock.time = 0.0f;
         dragonBones::WorldClock::clock.timeScale = 1.0f;
 
-        auto& allDragonBonesObjects = dragonBones::BaseObject::getAllObjects();
-
+        // Copy the dragonbones object vector since vector element will be deleted in BaseObject destructor.
+        std::vector<dragonBones::BaseObject*> allDragonBonesObjects = dragonBones::BaseObject::getAllObjects();
+        SE_LOGD("Starting to cleanup dragonbones object, count: %d\n", (int)allDragonBonesObjects.size());
         // Clear dragonBones::Armature objects those are not in the pool because
         // dragonBones::Armature controls life cycle for lots of other objects,
         // so it needs to be disposed first.
-//        SE_LOGD("before delete armature: %d\n", (int)__allDragonBonesObjects.size());
         for (auto dbObj : allDragonBonesObjects)
         {
             if (dynamic_cast<dragonBones::Armature*>(dbObj) != nullptr && !dbObj->isInPool())
@@ -480,6 +480,9 @@ bool register_all_dragonbones_manual(se::Object* obj)
         // Therefore, we clean object pool here.
         dragonBones::BaseObject::clearPool(0);
 
+        // Copy the dragonbones object vector again.
+        allDragonBonesObjects = dragonBones::BaseObject::getAllObjects();
+        SE_LOGD("After first cleanup, dragonbones object remained count: %d\n", (int)allDragonBonesObjects.size());
         // Check again whether there are some objects still in pool since the releationship of dragonbones objects is really complex.
         for (auto dbObj : allDragonBonesObjects)
         {
@@ -493,16 +496,18 @@ bool register_all_dragonbones_manual(se::Object* obj)
         // Clear pool again.
         dragonBones::BaseObject::clearPool(0);
 
-        SE_LOGD("all dragonbones object count: %d\n", (int)allDragonBonesObjects.size());
+        // Don't need to use copy operator since we only print leak object below.
+        auto& refAllDragonBonesObjects = dragonBones::BaseObject::getAllObjects();
+        SE_LOGD("After second cleanup, dragonbones object remained count: %d\n", (int)refAllDragonBonesObjects.size());
 
         // Print leak objects
-        for (auto dbObj : allDragonBonesObjects)
+        for (auto dbObj : refAllDragonBonesObjects)
         {
-            SE_LOGD("leak dragonbones object: %s, %p\n", typeid(*dbObj).name(), dbObj);
+            SE_LOGD("Leak dragonbones object: %s, %p\n", typeid(*dbObj).name(), dbObj);
         }
 
         // If there're leak objects, clear vector should be done for restarting game.
-        allDragonBonesObjects.clear();
+        refAllDragonBonesObjects.clear();
     });
 
     se::ScriptEngine::getInstance()->clearException();

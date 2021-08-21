72fe17423a942195347c719c36178b96885e22d7
timlyeee <timelyeee@gmail.com>
2021-08-21 17:01:39 +0800

    Revert "Director error fix"
    This reverts commit 78c10920706cc40e9878f08395e3a1fdbad5f006.

--- cocos/core/Director.h
+++ cocos/core/Director.h
@@ -419,8 +419,7 @@ public:
      * @zh 运行主循环
      */
     void tick(float dt);
-	Director();
-    ~Director() = default;
+
 private:
     static std::shared_ptr<Director> Instance;
 
@@ -433,7 +432,8 @@ private:
     std::vector<System*> _systems;
 
     scenegraph::Scene* _scene;
-    
+    Director();
+    ~Director();
     void _initOnRendererInitialized();
     //TODO: return Promise in js, c++ need adaption
     void _init();

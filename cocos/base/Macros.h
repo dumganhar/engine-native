e954705eea80310a7bd19f40b26f5a13d0e71ef6
dumganhar <dumganhar@gmail.com>
2021-08-25 14:07:08 +0800

    Add CC_DISALLOW_COPY_MOVE_ASSIGN macro.

--- cocos/base/Macros.h
+++ cocos/base/Macros.h
@@ -400,6 +400,15 @@ It should work same as apples CFSwapInt32LittleToHost(..)
         _Pragma("clang diagnostic pop")
 #endif
 
+#define CC_DISALLOW_ASSIGN(TypeName)                \
+    TypeName &operator=(const TypeName &) = delete; \
+    TypeName &operator=(TypeName &&) = delete
+
+#define CC_DISALLOW_COPY_MOVE_ASSIGN(TypeName) \
+    TypeName(const TypeName &) = delete;       \
+    TypeName(TypeName &&)      = delete;       \
+    CC_DISALLOW_ASSIGN(TypeName)
+
 #define ENABLE_COPY_SEMANTICS(cls) \
     cls(const cls &) = default;    \
     cls &operator=(const cls &) = default;
@@ -604,9 +613,9 @@ It should work same as apples CFSwapInt32LittleToHost(..)
 #define ENABLE_IF_T3_RET(t1, t2, t3) std::enable_if_t<std::is_same<t1, T>::value || std::is_same<t2, T>::value || std::is_same<t3, T>::value, void>
 
 #if defined(__GNUC__) && __GNUC__ >= 4
-    #define CC_PREDICT_TRUE(x)   __builtin_expect(!!(x), 1)
+    #define CC_PREDICT_TRUE(x)  __builtin_expect(!!(x), 1)
     #define CC_PREDICT_FALSE(x) __builtin_expect(!!(x), 0)
 #else
-    #define CC_PREDICT_TRUE(x)   (x)
+    #define CC_PREDICT_TRUE(x)  (x)
     #define CC_PREDICT_FALSE(x) (x)
 #endif

c8315b4d595efb5b1292815d8c8d2e40595e666b
minggo <nudtzxm@gmail.com>
2021-08-30 21:35:46 +0800

    format codes

--- cocos/scene/Pass.h
+++ cocos/scene/Pass.h
@@ -257,7 +257,7 @@ public:
     gfx::Shader *getShaderVariant(const std::vector<IMacroPatch> &patches);
 
     // infos
-    inline Root *                                                             getRoot() const { return _root; }
+    inline Root *       getRoot() const { return _root; }
     inline gfx::Device *getDevice() const { return _device; }
     //    inline const IProgramInfo &                                               getShaderInfo() const { return _shaderInfo; }
     //    inline gfx::DescriptorSetLayout *                                              getLocalSetLayout() const { return programLib.getDescriptorSetLayout(_device, _programName, true); }

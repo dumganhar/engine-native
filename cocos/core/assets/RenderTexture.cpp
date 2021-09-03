89294752209b8073903bba8efa4204c27cc9f98e
mmyduckx <308303735@qq.com>
2021-09-03 20:07:24 +0800

    RenderTExture implemetation

--- cocos/core/assets/RenderTexture.cpp
+++ cocos/core/assets/RenderTexture.cpp
@@ -24,21 +24,119 @@
 ****************************************************************************/
 
 #include "core/assets/RenderTexture.h"
+#include "core/Root.h"
 #include "core/utils/IDGenerator.h"
+#include "renderer/gfx-base/GFXDef-common.h"
 
+namespace {
+uint64_t getDefaultSamlerHash() {
+    auto defaultInfo            = cc::gfx::SamplerInfo();
+    defaultInfo.cmpFunc         = cc::gfx::ComparisonFunc::NEVER; //defaultInfo.cmpFunc seems diffent between ts and cpp
+    uint64_t defaultSamplerHash = cc::pipeline::SamplerLib::genSamplerHash(defaultInfo);
+    return defaultSamplerHash;
+}
+
+cc::gfx::RenderPassInfo getPassInfo() {
+    auto passInfo = cc::gfx::RenderPassInfo();
+    for (auto &colorAttachment : passInfo.colorAttachments) {
+        colorAttachment.endAccesses = std::vector<cc::gfx::AccessType>{cc::gfx::AccessType::COLOR_ATTACHMENT_WRITE};
+    }
+    return passInfo;
+}
+
+cc::scene::IRenderWindowInfo windowInfo{
+    .width          = 1,
+    .height         = 1,
+    .renderPassInfo = getPassInfo()};
+} // namespace
 namespace cc {
 
-static IDGenerator _idGenerator("RenderTex");
+static IDGenerator idGenerator("RenderTex");
 
-//const _colorAttachment = new ColorAttachment();
-//_colorAttachment.endAccesses = [AccessType.FRAGMENT_SHADER_READ_TEXTURE];
-//const _depthStencilAttachment = new DepthStencilAttachment();
-//const passInfo = new RenderPassInfo([_colorAttachment], _depthStencilAttachment);
-//
-//const _windowInfo: IRenderWindowInfo = {
-//    width: 1,
-//    height: 1,
-//    renderPassInfo: passInfo,
-//};
+void RenderTexture::initialize(const IRenderTextureCreateInfo &info) {
+    _name   = info.name.value();
+    _width  = info.width;
+    _height = info.height;
+    initWindow(info);
+}
+
+void RenderTexture::reset(const IRenderTextureCreateInfo &info) {
+    initialize(info);
+}
+
+bool RenderTexture::destroy() {
+    if (_window != nullptr) {
+        Root::getInstance()->destroyWindow(_window);
+        _window = nullptr;
+    }
+    return Super::destroy();
+}
+
+void RenderTexture::resize(uint32_t width, uint32_t height) {
+    _width  = width;
+    _height = height;
+    if (_window != nullptr) {
+        _window->resize(_width, _height);
+    }
+    // emit(std::string("resize"), _window); //cjh TODO: not inherit form Eventify
+}
+
+gfx::Texture *RenderTexture::getGFXTexture() {
+    return _window ? _window->getFramebuffer()->getColorTextures()[0] : nullptr;
+}
+
+gfx::Sampler *RenderTexture::getGFXSampler() {
+    return pipeline::SamplerLib::getSampler(getDefaultSamlerHash());
+}
+
+uint64_t RenderTexture::getSamplerHash() const {
+    return getDefaultSamlerHash();
+}
+
+void RenderTexture::onLoaded() {
+    initWindow();
+}
+
+void RenderTexture::initWindow() {
+    windowInfo.title          = _name;
+    windowInfo.width          = _width;
+    windowInfo.height         = _height;
+    windowInfo.renderPassInfo = getPassInfo();
+
+    if (_window != nullptr) {
+        _window->destroy();
+        _window->initialize(Root::getInstance()->getDevice(), windowInfo);
+    } else {
+        _window = Root::getInstance()->createWindow(windowInfo);
+    }
+}
+
+void RenderTexture::initWindow(const IRenderTextureCreateInfo &info) {
+    windowInfo.title  = _name;
+    windowInfo.width  = _width;
+    windowInfo.height = _height;
+    if (info.passInfo.has_value()) {
+        windowInfo.renderPassInfo = info.passInfo.value();
+    } else {
+        windowInfo.renderPassInfo = getPassInfo();
+    }
+    if (_window != nullptr) {
+        _window->destroy();
+        _window->initialize(Root::getInstance()->getDevice(), windowInfo);
+    } else {
+        _window = Root::getInstance()->createWindow(windowInfo);
+    }
+}
+
+void RenderTexture::initDefault(const std::optional<std::string> &uuid /* = {}*/) {
+    Super::initDefault(uuid);
+    _width  = 1;
+    _height = 1;
+    initWindow();
+}
+
+bool RenderTexture::validate() const {
+    return _width >= 1 && _width <= 2048 && _height >= 1 && _height <= 2048;
+}
 
 } // namespace cc

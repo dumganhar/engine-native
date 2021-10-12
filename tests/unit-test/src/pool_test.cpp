4d5823d015e6951d8663a0b062df73f75e15340a
mmyduckx <308303735@qq.com>
2021-10-12 14:49:49 +0800

    updade License

--- tests/unit-test/src/pool_test.cpp
+++ tests/unit-test/src/pool_test.cpp
@@ -1,26 +1,27 @@
 /****************************************************************************
-Copyright (c) 2021 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2021 Xiamen Yaji Software Co., Ltd.
 
-http://www.cocos2d-x.org
+ http://www.cocos.com
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
 #include "core/memop/Pool.h"
 #include "gtest/gtest.h"
 
@@ -54,22 +55,21 @@ TEST(PoolTest, alloc) {
 }
 
 TEST(PoolTest, free) {
-    auto * tmp = createTest();
+    auto *tmp = createTest();
     pool->free(tmp);
-    auto * newElement = pool->alloc();
+    auto *newElement = pool->alloc();
     EXPECT_EQ(newElement, tmp);
 }
 
-TEST(PoolTest, freeArray) { 
-    const int32_t TEST_SIZE = 5;
+TEST(PoolTest, freeArray) {
+    const int32_t           TEST_SIZE = 5;
     std::vector<PoolTest *> tmpArr(TEST_SIZE);
-   for(int32_t i = 0; i < TEST_SIZE; ++i) {
-       PoolTest* a = createTest();
-   }
+    for (int32_t i = 0; i < TEST_SIZE; ++i) {
+        PoolTest *a = createTest();
+    }
     pool->freeArray(tmpArr);
-    for(int32_t i = 0; i < TEST_SIZE; ++i) {
-        auto * tmpObj = pool->alloc();
+    for (int32_t i = 0; i < TEST_SIZE; ++i) {
+        auto *tmpObj = pool->alloc();
         EXPECT_EQ(tmpArr[TEST_SIZE - i - 1], tmpObj);
     }
-
 }

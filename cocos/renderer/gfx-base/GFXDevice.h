19479e0d3800d46173204846c42e4d0d39b0fa2d
minggo <nudtzxm@gmail.com>
2021-12-28 17:21:27 +0800

    use SharedPtr to fix crash

--- cocos/renderer/gfx-base/GFXDevice.h
+++ cocos/renderer/gfx-base/GFXDevice.h
@@ -165,12 +165,12 @@ protected:
     uint32_t     _numTriangles{0U};
     MemoryStatus _memoryStatus;
 
-    unordered_map<SamplerInfo, Sampler *, Hasher<SamplerInfo>>                      _samplers;
-    unordered_map<GlobalBarrierInfo, GlobalBarrier *, Hasher<GlobalBarrierInfo>>    _globalBarriers;
-    unordered_map<TextureBarrierInfo, TextureBarrier *, Hasher<TextureBarrierInfo>> _textureBarriers;
+    unordered_map<SamplerInfo, SharedPtr<Sampler>, Hasher<SamplerInfo>>                      _samplers;
+    unordered_map<GlobalBarrierInfo, SharedPtr<GlobalBarrier>, Hasher<GlobalBarrierInfo>>    _globalBarriers;
+    unordered_map<TextureBarrierInfo, SharedPtr<TextureBarrier>, Hasher<TextureBarrierInfo>> _textureBarriers;
 
 private:
-    vector<Swapchain *> _swapchains;
+    vector<SharedPtr<Swapchain>> _swapchains;
 };
 
 //////////////////////////////////////////////////////////////////////////
